a7bd6e1bb3f19157cd9e6e62c787cb095c3c6f43
nostreets <nileoverstreet@gmail.com>
2017-10-18 19:09:27 -0700

    NostreetsExtensions Abstraction

--- EFDBService.cs
+++ EFDBService.cs
@@ -1,26 +1,37 @@
-﻿using NostreetsORM.Interfaces;
+﻿using NostreetsEntities.Utilities;
+using NostreetsORM.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace NostreetsEntities
 {
-    public class EFDBService<T> : IDBService<T> where T : class
+    public class EFDBService<T> : IEFDBService<T> where T : class
     {
 
         public EFDBService()
         {
-
+            if (!CheckIfTypeIsValid()) { throw new Exception("Type has to have a property called Id"); }
         }
 
         public EFDBService(string connectionKey)
         {
+            if (!CheckIfTypeIsValid()) { throw new Exception("Type has to have a property called Id"); }
+
             _connectionKey = connectionKey;
         }
 
         private string _connectionKey = "DefaultConnection";
         private EFDBContext<T> _context = null;
+
+        private bool CheckIfTypeIsValid()
+        {
+            return (typeof(T).GetProperties().FirstOrDefault(a => a.Name == "Id") != null) ? true : false;
+        }
 
         public List<T> GetAll()
         {
@@ -34,10 +45,16 @@ namespace NostreetsEntities
 
         public T Get(object id)
         {
+            Func<T, bool> predicate = a => a.GetType().GetProperty("Id").GetValue(a) == id;
+            return Get(predicate);
+        }
+
+        public T Get(Func<T, bool> predicate)
+        {
             T result = null;
             using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
             {
-                result = _context.Table.FirstOrDefault(a => a.GetType().GetProperties().GetValue(0) == id);
+                result = _context.Table.FirstOrDefault(predicate);
             }
             return result;
         }
@@ -71,9 +88,17 @@ namespace NostreetsEntities
 
         public void Delete(object id)
         {
+            //Delete(ExpressionBuilder.GetPredicate<T>(new[] { new Filter("Id", Op.Equals, id) }));
+
+            Func<T, bool> predicate = a => a.GetType().GetProperty("Id").GetValue(a) == id;
+            Delete(predicate);
+        }
+
+        public void Delete(Func<T, bool> predicate)
+        {
             using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
             {
-                T obj = _context.Table.FirstOrDefault(a => a.GetType().GetProperties().GetValue(0) == id);
+                T obj = _context.Table.FirstOrDefault(predicate);
 
                 _context.Table.Remove(obj);
 
@@ -83,9 +108,150 @@ namespace NostreetsEntities
 
         public void Update(T model)
         {
+            Func<T, bool> predicate = a => a.GetType().GetProperty("Id").GetValue(a) == model.GetType().GetProperty("Id").GetValue(model);
+            Update(predicate, model);
+        }
+
+        public void Update(Func<T, bool> predicate, T model)
+        {
             using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
             {
-                T targetedUser = _context.Table.FirstOrDefault(a => a.GetType().GetProperties().GetValue(0) == model.GetType().GetProperties().GetValue(0));
+                _context.Table.Attach(model);
+                _context.Entry(model).State = EntityState.Modified;
+
+                if (_context.SaveChanges() == 0) { throw new Exception("DB changes not saved!"); }
+            }
+        }
+
+        public List<T> Where(Func<T, bool> predicate)
+        {
+            List<T> result = null;
+            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
+            {
+                result = _context.Table.Where(predicate).ToList();
+            }
+            return result;
+        }
+
+        public List<T> Where(Func<T, int, bool> predicate)
+        {
+            List<T> result = null;
+            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
+            {
+                result = _context.Table.Where(predicate).ToList();
+            }
+            return result;
+        }
+
+    }
+
+    public class EFDBService<T, IdType> : IEFDBService<T, IdType> where T : class
+    {
+
+        public EFDBService()
+        {
+            if (!CheckIfTypeIsValid()) { throw new Exception("Type has to have a property called Id"); }
+        }
+
+        public EFDBService(string connectionKey)
+        {
+            if (!CheckIfTypeIsValid()) { throw new Exception("Type has to have a property called Id"); }
+
+            _connectionKey = connectionKey;
+        }
+
+        private string _connectionKey = "DefaultConnection";
+        private EFDBContext<T> _context = null;
+
+        private bool CheckIfTypeIsValid()
+        {
+            return (typeof(T).GetProperties().FirstOrDefault(a => a.Name == "Id") != null) ? true : false;
+        }
+
+        public List<T> GetAll()
+        {
+            List<T> result = null;
+            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
+            {
+                result = _context.Table.ToList();
+            }
+            return result;
+        }
+
+        public T Get(IdType id)
+        {
+            Func<T, bool> predicate = a => a.GetType().GetProperty("Id").GetValue(a) == (object)id;
+            return Get(predicate);
+        }
+
+        public T Get(Func<T, bool> predicate)
+        {
+            T result = null;
+            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
+            {
+                result = _context.Table.FirstOrDefault(predicate);
+            }
+            return result;
+        }
+
+        public IdType Insert(T model)
+        {
+            IdType result = default(IdType);
+
+            var firstProp = model.GetType().GetProperties()[0];
+
+            if (firstProp.PropertyType.Name.Contains("Int"))
+            {
+                model.GetType().GetProperty(firstProp.Name).SetValue(model, GetAll().Count + 1);
+            }
+            else if (firstProp.PropertyType.Name == "GUID")
+            {
+                model.GetType().GetProperties().SetValue(Guid.NewGuid().ToString(), 0);
+            }
+
+            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
+            {
+                _context.Table.Add(model);
+
+                if (_context.SaveChanges() == 0) { throw new Exception("DB changes not saved!"); }
+
+                result = (IdType)model.GetType().GetProperties().GetValue(0);
+            }
+
+            return result;
+        }
+
+        public void Delete(IdType id)
+        {
+            //Delete(ExpressionBuilder.GetPredicate<T>(new[] { new Filter("Id", Op.Equals, id) }));
+
+            Func<T, bool> predicate = a => a.GetType().GetProperty("Id").GetValue(a) == (object)id;
+            Delete(predicate);
+        }
+
+        public void Delete(Func<T, bool> predicate)
+        {
+            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
+            {
+                T obj = _context.Table.FirstOrDefault(predicate);
+
+                _context.Table.Remove(obj);
+
+                if (_context.SaveChanges() == 0) { throw new Exception("DB changes not saved!"); }
+            }
+        }
+
+        public void Update(T model)
+        {
+            Func<T, bool> predicate = a => a.GetType().GetProperty("Id").GetValue(a) == model.GetType().GetProperty("Id").GetValue(model);
+            Update(predicate, model);
+        }
+
+        public void Update(Func<T, bool> predicate, T model)
+        {
+            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
+            {
+                T targetedUser = _context.Table.FirstOrDefault(predicate);
                 targetedUser = model;
 
                 if (_context.SaveChanges() == 0) { throw new Exception("DB changes not saved!"); }
@@ -114,122 +280,19 @@ namespace NostreetsEntities
 
     }
 
-    public class EFDBService<T, IdType> : IDBService<T, IdType> where T : class
-    {
-
-        public EFDBService()
-        {
-        }
-
-        public EFDBService(string connectionKey)
-        {
-            _connectionKey = connectionKey;
-        }
-
-        private string _connectionKey = "DefaultConnection";
-        private EFDBContext<T> _context = null;
-
-        public List<T> GetAll()
-        {
-            List<T> result = null;
-            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
-            {
-                result = _context.Table.ToList();
-            }
-            return result;
-        }
-
-        public T Get(IdType id)
-        {
-            T result = null;
-            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
-            {
-                result = _context.Table.FirstOrDefault(a => a.GetType().GetProperties().GetValue(0) == (object)id);
-            }
-            return result;
-        }
-
-        public IdType Insert(T model)
-        {
-            IdType result = default(IdType);
-            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
-            {
-                var firstPTypeName = model.GetType().GetProperties()[0].GetType().Name;
-
-                if (firstPTypeName.Contains("INT"))
-                {
-                    model.GetType().GetProperties().SetValue(GetAll().Count + 1, 0);
-                }
-                else if (firstPTypeName == "GUID")
-                {
-                    model.GetType().GetProperties().SetValue(Guid.NewGuid().ToString(), 0);
-                }
-                _context.Table.Add(model);
-
-                if (_context.SaveChanges() == 0) { throw new Exception("DB changes not saved!"); }
-
-                result = (IdType)model.GetType().GetProperties().GetValue(0);
-            }
-
-            return result;
-        }
-
-        public void Delete(IdType id)
-        {
-            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
-            {
-                T obj = _context.Table.FirstOrDefault(a => a.GetType().GetProperties().GetValue(0) == (object)id);
-
-                _context.Table.Remove(obj);
-
-                if (_context.SaveChanges() == 0) { throw new Exception("DB changes not saved!"); }
-            }
-        }
-
-        public void Update(T model)
-        {
-            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
-            {
-                T targetedUser = _context.Table.FirstOrDefault(a => a.GetType().GetProperties().GetValue(0) == model.GetType().GetProperties().GetValue(0));
-                targetedUser = model;
-
-                if (_context.SaveChanges() == 0) { throw new Exception("DB changes not saved!"); }
-            }
-        }
-
-        public IEnumerable<T> Where(Func<T, bool> predicate)
-        {
-            IEnumerable<T> result = null;
-            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
-            {
-                result = _context.Table.Where(predicate);
-            }
-            return result;
-        }
-
-        public IEnumerable<T> Where(Func<T, int, bool> predicate)
-        {
-            IEnumerable<T> result = null;
-            using (_context = new EFDBContext<T>(_connectionKey, typeof(T).Name))
-            {
-                result = _context.Table.Where(predicate);
-            }
-            return result;
-        }
-
-    }
-
     public class EFDBContext<TContext> : DbContext where TContext : class
     {
         public EFDBContext()
             : base("DefaultConnection")
         {
+            OnModelCreating(new DbModelBuilder().HasDefaultSchema(nameof(TContext)));
             //if (ChangeTracker.HasChanges()) { Database.SetInitializer(new MigrateDatabaseToLatestVersion<EFDBContext<TContext>, MigrationConfiguration<TContext>>()); }
         }
 
         public EFDBContext(string connectionKey)
             : base(connectionKey)
         {
+            OnModelCreating(new DbModelBuilder().HasDefaultSchema(nameof(TContext)));
             //if (this.IsChanged()) { Database.SetInitializer(new MigrateDatabaseToLatestVersion<EFDBContext<TContext>, MigrationConfiguration<TContext>>()); }
         }
 
@@ -241,6 +304,25 @@ namespace NostreetsEntities
         }
 
         public IDbSet<TContext> Table { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Func<Type, bool> modelConfigPredicate = type => type.BaseType.IsGenericType && type.BaseType.GetGenericTypeDefinition() == typeof(EntityTypeConfiguration<>);
+
+            List<Type> typesToRegister = new List<Type>();
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                typesToRegister.AddRange(assembly.GetTypes().Where(modelConfigPredicate));
+            }
+
+            foreach (Type type in typesToRegister)
+            {
+                dynamic configurationInstance = Activator.CreateInstance(type);
+                modelBuilder.Configurations.Add(configurationInstance);
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
